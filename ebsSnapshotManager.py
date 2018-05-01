709557c79018f742a1a478605a342a8a34472a86
tnynyn <33666123+tnynyn@users.noreply.github.com>
2018-05-01 12:38:27 -0700

    Update ebsSnapshotManager.py
    Updated script for better tag handling

--- ebsSnapshotManager.py
+++ ebsSnapshotManager.py
@@ -5,64 +5,52 @@ import datetime
 source_region = 'us-west-2' 
 copy_region = 'us-east-2'
 ec2_client = boto3.client('ec2',region_name='us-west-2')
-retention_days = 30
+ec2_resource = boto3.resource('ec2')
 
 def lambda_handler(event, context):
     # Get all volume matching the tag Backup:Yes
     volumes = ec2_client.describe_volumes(Filters = [
-           {
-               'Name' : "tag-key",
-               'Values' : ["Backup"]
-           },
-           {
-               'Name' : "tag-value",
-               'Values' : ['Yes']
-           }
+           { 'Name' : "tag-key", 'Values' : ["Backup"] },
+           { 'Name' : "tag-value", 'Values' : ['yes'] }
        ])
        
-    to_tag = collections.defaultdict(list)
-     
-    # Iterate over all volumes with matching tags and create snapshot for the specific volume using volume_id
-    for volume in volumes.get('Volumes',[]):
-        volume_id = volume.get('VolumeId')
-        try:
-            retention_days = [
-                int(t.get('Value')) for t in volume['Tags']
-                if t['Key'] == 'Retention'][0]                                              
-        except IndexError:
-            retention_days = 30
+    # Iterate over all volumes with matching tags
+    for volume in volumes['Volumes']:
+        volume_id = volume['VolumeId']
         
-        try:
-            volume_name = [
-                str(t.get('Value')) for t in volume['Tags']
-                if t['Key'] == 'Name'][0] 
-        except IndexError:
-                volume_name = 'None'
+        # Create snapshot
+        snapshot = ec2_client.create_snapshot(VolumeId = volume_id, Description = "Created by lambda function ebsSnapshotManager") 
+        snapshot_id = snapshot['SnapshotId']
         
-        snapshot = ec2_client.create_snapshot(VolumeId = volume_id, Description = "Created by lambda function ebsSnapshotManager")  
+        # Get snapshot tag info
+        snap = ec2_resource.Snapshot(snapshot['SnapshotId'])
+        if 'Tags' in volume:
+            for tags in volume['Tags']:
+                if tags["Key"] == 'Name':
+                    volume_name = tags["Value"]
+                
+                try:
+                    if tags["Key"] == 'Retention':
+                        retention_days = int(tags["Value"])
+                except ValueError:
+                    retention_days = int(30)
+                    
+        delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+        delete_fmt = delete_date.strftime('%Y-%m-%d')
+        today_fmt = datetime.date.today().strftime('%Y-%m-%d')
         
-        if snapshot:
-            print "\tSNAPSHOT [%s] created from [%s] [%s]" % ( snapshot['SnapshotId'], volume_name, volume_id )
-            to_tag[retention_days].append(snapshot['SnapshotId'])
-            #print "\tRetaining SNAPSHOT [%s] for [%d] days" % ( snapshot['SnapshotId'], retention_days )            
-
-        # Tags snapshot to delete on date   
-        for retention_days in to_tag.keys():
-            delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
-            delete_fmt = delete_date.strftime('%Y-%m-%d')
-            today_fmt = datetime.date.today().strftime('%Y-%m-%d')
-
-        #print "\tDeleting [%d] SNAPSHOT(S) on [%s]" % (len(to_tag[retention_days]), delete_fmt)
-        ec2_client.create_tags(
-            Resources=to_tag[retention_days],
+        #Tags snapshot
+        snap.create_tags(
             Tags=[
                 { 'Key': 'CreatedOn', 'Value': today_fmt },
                 { 'Key': 'DeleteOn', 'Value': delete_fmt },
                 { 'Key': 'Type', 'Value': 'Automated' },
-                { 'Key': 'Name', 'Value': volume_name },
+                { 'Key': 'Name', 'Value': volume_name }
             ]
-        )                     
-                          
+        )
+        
+        print "\tSNAPSHOT [%s] created for [%s]:[%s]" % (snapshot_id, volume_name, volume_id)
+                
         # Deletes snapshot based on "delete_on" date for primary region 
         delete_on = datetime.date.today().strftime('%Y-%m-%d')
             # limit snapshots to process to ones marked for deletion on this day
@@ -74,5 +62,5 @@ def lambda_handler(event, context):
         ]
         snapshot_response = ec2_client.describe_snapshots(Filters=filters)       
         for snapshot in snapshot_response['Snapshots']:
-            print "\tDeleting SNAPSHOT [%s]" % snapshot['SnapshotId']
             ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+            print "\SNAPSHOT [%s] deleted due to retention policy" % (snapshot['SnapshotId'])

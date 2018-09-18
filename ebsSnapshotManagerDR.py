a0026d6944e0b0b5c0fd1e9927faee6bd6108703
tnynyn <33666123+tnynyn@users.noreply.github.com>
2018-09-18 09:15:41 -0700

    Updated script to copy all marked snapshots
    Marked snapshots will be from ebsSnapshotManager script; will unmark snapshots once it is copied to DR; does not handle any retention

--- ebsSnapshotManagerDR.py
+++ ebsSnapshotManagerDR.py
@@ -1,50 +1,49 @@
 import boto3
 import datetime
-
 source_region = 'us-west-2' 
 copy_region = 'us-east-2'
 ec = boto3.client('ec2')
 addl_ec = boto3.client('ec2', region_name=copy_region)
 
 def lambda_handler(event, context):
-
-    #Copy snapshot(s) based on snapshots created "today"
-    today_fmt = datetime.date.today().strftime('%Y-%m-%d')
+    #Get count of concurrent CopySnapshot operation in copy_region
+    i = 0
     filters = [
-        { 'Name': 'tag:CreatedOn', 'Values': [today_fmt] },
+        { 'Name': 'status', 'Values': ['pending'] },
+    ]
+    snapshot_response = addl_ec.describe_snapshots(Filters=filters)
+    for snap in snapshot_response['Snapshots']:
+        if i == 5:
+            print "\tWARNING: Five concurrent CopySnapshot operation in progess, exiting..."
+            raise SystemExit
+    i = i + 1
+    #Filter snapshots to be copied from PR to DR
+    filters = [
         { 'Name': 'tag:Type', 'Values': ['Automated'] },
+        { 'Name': 'tag:DR', 'Values': ['No'] },
     ]
     snapshot_response = ec.describe_snapshots(Filters=filters)
-    i = 0 #Counter for AWS limitation of five concurrent CopySnapshot operations
     for snap in snapshot_response['Snapshots']:
         tags=snap['Tags']
-        for tag in tags:
+        for tag in tags:  #Create variables for tagging snapshots in DR
             if tag['Key'] == 'CreatedOn':
                 created_on = tag['Value']
             if tag['Key'] == 'DeleteOn':
                 delete_on = tag['Value']
             if tag['Key'] == 'Name':
                 volume_name = tag['Value']
-        if i < 5:         
-            #Check snapshot status
+        if i < 5:
+            #Check for snapshots currently being created
             if snap['State'] == 'pending':
-                print "\tWARNING: [%s] of [%s] is pending and will not be copied" % ( snap['SnapshotId'], volume_name )
-                #Mark Snapshot not copied
-                ec.create_tags(
-                    Resources=[snap['SnapshotId']],
-                    Tags=[
-                        { 'Key': 'DR', 'Value': 'No' },
-                    ]
-                )
-                continue
-                          
+                print "\tWARNING: [%s] of [%s] under creation and will not be copied" % ( snap['SnapshotId'], volume_name )
+            #Copies snapshots to DR                
             addl_snap = addl_ec.copy_snapshot(
                 SourceRegion=source_region,
                 SourceSnapshotId=snap['SnapshotId'],
                 Description='Original Snapshot ID: ' + snap['SnapshotId'],
                 DestinationRegion=copy_region
             )
-
+            #Creates tags for snaphots in DR
             addl_ec.create_tags(
                 Resources=[addl_snap['SnapshotId']],
                 Tags=[
@@ -54,29 +53,15 @@ def lambda_handler(event, context):
                     { 'Key': 'Name', 'Value': volume_name },
                 ]
             )
-            i = i + 1
-            print "\tSNAPSHOT [%s] copied from [%s] to [%s]" % ( snap['SnapshotId'], source_region, copy_region )
-        else: 
-            print "\tWARNING: Five concurrent CopySnapshot operation limit reached, [%s] of [%s] will not be copied" % ( snap['SnapshotId'], volume_name )
-            #Mark Snapshot not copied
+            #Mark snapshots that has been copied to DR
             ec.create_tags(
                 Resources=[snap['SnapshotId']],
                 Tags=[
-                    { 'Key': 'DR', 'Value': 'No' },
-               ]
-            )
-            continue
+                    { 'Key': 'DR', 'Value': '' },
+                ]
+            )   
+            i = i + 1
+            print "\tSNAPSHOT [%s] of [%s] copied from [%s] to [%s]" % ( snap['SnapshotId'], volume_name, source_region, copy_region )
+        else: 
+            print "\tWARNING: Five concurrent CopySnapshot operation limit reached, [%s] of [%s] will not be copied" % ( snap['SnapshotId'], volume_name )
     
-    delete_on = datetime.date.today().strftime('%Y-%m-%d')
-        # limit snapshots to process to ones marked for deletion on this day
-        # AND limit snapshots to process to ones that are automated only
-        # AND exclude automated snapshots marked for permanent retention
-    filters = [
-        { 'Name': 'tag:DeleteOn', 'Values': [delete_on] },
-        { 'Name': 'tag:Type', 'Values': ['Automated'] },
-    ]
-    snapshot_response = addl_ec.describe_snapshots(Filters=filters)
-
-    for snap in snapshot_response['Snapshots']:
-        print "\tDeleting SNAPSHOT %s" % snap['SnapshotId']
-        addl_ec.delete_snapshot(SnapshotId=snap['SnapshotId'])

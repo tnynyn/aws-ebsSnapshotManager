03d20ea76c067e0336d4490b92d463715e59fcb8
tnynyn <33666123+tnynyn@users.noreply.github.com>
2019-12-17 11:31:29 -0800

    updated print syntax for python3

--- ebsSnapshotManager.py
+++ ebsSnapshotManager.py
@@ -1,11 +1,11 @@
 import boto3
 import collections
 import datetime
+
 source_region = 'us-west-2' 
 copy_region = 'us-east-2'
-ec2_client = boto3.client('ec2',region_name=source_region)
+ec2_client = boto3.client('ec2',region_name='us-west-2')
 ec2_resource = boto3.resource('ec2')
-addl_ec = boto3.client('ec2', region_name=copy_region)
 
 def lambda_handler(event, context):
     # Get all volume matching the tag Backup:Yes
@@ -13,45 +13,54 @@ def lambda_handler(event, context):
            { 'Name' : "tag-key", 'Values' : ["Backup"] },
            { 'Name' : "tag-value", 'Values' : ['Yes'] }
        ])
+       
+    # Iterate over all volumes with matching tags
     for volume in volumes['Volumes']:
-        volume_id = volume['VolumeId']        
+        volume_id = volume['VolumeId']
+        
         # Create snapshot
         snapshot = ec2_client.create_snapshot(VolumeId = volume_id, Description = "Created by lambda function ebsSnapshotManager") 
-        snapshot_id = snapshot['SnapshotId']       
+        snapshot_id = snapshot['SnapshotId']
+        
         # Get snapshot tag info
         snap = ec2_resource.Snapshot(snapshot['SnapshotId'])
-        volume_name = ''
-        retention_days = int(30)
-        for tags in volume['Tags']:
-            if tags["Key"] == 'Name':
-                volume_name = tags["Value"]               
-            if tags["Key"] == 'Retention':
-                retention_days = int(tags["Value"])
-        #Tags snapshot            
+        if 'Tags' in volume:
+            for tags in volume['Tags']:
+                if tags["Key"] == 'Name':
+                    volume_name = tags["Value"]
+                
+                try:
+                    if tags["Key"] == 'Retention':
+                        retention_days = int(tags["Value"])
+                except ValueError:
+                    retention_days = int(30)
+
         delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
         delete_fmt = delete_date.strftime('%Y-%m-%d')
-        today_fmt = datetime.date.today().strftime('%Y-%m-%d')        
+        today_fmt = datetime.date.today().strftime('%Y-%m-%d')
+        
+        #Tags snapshot
         snap.create_tags(
             Tags=[
                 { 'Key': 'CreatedOn', 'Value': today_fmt },
                 { 'Key': 'DeleteOn', 'Value': delete_fmt },
                 { 'Key': 'Type', 'Value': 'Automated' },
-                { 'Key': 'Name', 'Value': volume_name },
-                { 'Key': 'DR', 'Value': 'No' }
+                { 'Key': 'Name', 'Value': volume_name }
             ]
-        )        
-        print "\tSNAPSHOT [%s] created for [%s]:[%s]" % (snapshot_id, volume_name, volume_id)             
-    # Deletes snapshot based on "delete_on" date for PR and DR regions 
-    delete_on = datetime.date.today().strftime('%Y-%m-%d')
-    filters = [
-        { 'Name': 'tag:DeleteOn', 'Values': [delete_on] },
-        { 'Name': 'tag:Type', 'Values': ['Automated'] },
-    ]
-    snapshot_response = ec2_client.describe_snapshots(Filters=filters)       
-    for snapshot in snapshot_response['Snapshots']:
-        ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
-        print "\tSNAPSHOT [%s] deleted due to retention policy" % (snapshot['SnapshotId'])           
-    snapshot_responseDR = addl_ec.describe_snapshots(Filters=filters)
-    for snapshotDR in snapshot_responseDR['Snapshots']:
-        addl_ec.delete_snapshot(SnapshotId=snapshotDR['SnapshotId'])
-        print "\tSNAPSHOT_DR [%s] deleted due to retention policy" % (snapshotDR['SnapshotId'])
+        )
+        
+        print ("SNAPSHOT %s created for %s : %s" % (snapshot_id, volume_name, volume_id)
+
+        # Deletes snapshot based on "delete_on" date for primary region 
+        delete_on = datetime.date.today().strftime('%Y-%m-%d')
+            # limit snapshots to process to ones marked for deletion on this day
+            # AND limit snapshots to process to ones that are automated only
+            # AND exclude automated snapshots marked for permanent retention
+        filters = [
+            { 'Name': 'tag:DeleteOn', 'Values': [delete_on] },
+            { 'Name': 'tag:Type', 'Values': ['Automated'] },
+        ]
+        snapshot_response = ec2_client.describe_snapshots(Filters=filters)       
+        for snapshot in snapshot_response['Snapshots']:
+            ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+            print ("SNAPSHOT %s deleted due to retention policy" % (snapshot['SnapshotId'])
